14a34d28c700c3eee2a8a1fde4e8bced793fb5fa
David Ko <david.dk.ko@gmail.com>
2012-06-01 10:57:41 -0700

    Initial Commit

---
+++ RoboMancer.cpp
@@ -0,0 +1,37 @@
+#include <gtk/gtk.h>
+#include <string.h>
+
+/* These store the embedded glade xml file */
+extern const char _binary_interface_interface_glade_start[];
+extern size_t _binary_interface_interface_glade_size;
+extern const char _binary_interface_interface_glade_end[];
+
+GtkBuilder *g_builder;
+GtkWidget *g_window;
+
+int main(int argc, char* argv[])
+{
+  GError *error = NULL;
+
+  gtk_init(&argc, &argv);
+
+  /* Create the GTK Builder */
+  g_builder = gtk_builder_new();
+
+  /* Load the UI */
+  if( ! gtk_builder_add_from_string(g_builder, _binary_interface_interface_glade_start, strlen(_binary_interface_interface_glade_start), &error) )
+  {
+    g_warning("%s", error->message);
+    //g_free(error);
+    return -1;
+  }
+
+  /* Get the main window */
+  g_window = GTK_WIDGET( gtk_builder_get_object(g_builder, "window1"));
+  /* Connect signals */
+  gtk_builder_connect_signals(g_builder, NULL);
+  /* Show the window */
+  gtk_widget_show(g_window);
+  gtk_main();
+  return 0;
+}
