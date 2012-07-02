605a981fe46792bbd2e137c5a2a816404a6bd3d0
David Ko <david.dk.ko@gmail.com>
2012-07-02 15:30:40 -0700

    Modified default save filename

--- teachingDialog.cpp
+++ teachingDialog.cpp
@@ -118,7 +118,7 @@ void on_button_saveToProgram_clicked(GtkWidget*widget, gpointer data)
       GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
       NULL);
   gtk_file_chooser_set_do_overwrite_confirmation (GTK_FILE_CHOOSER (dialog), TRUE);
-  gtk_file_chooser_set_current_name (GTK_FILE_CHOOSER (dialog), "Untitled document.cpp");
+  gtk_file_chooser_set_current_name (GTK_FILE_CHOOSER (dialog), "UntitledDocument.cpp");
   if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT)
   {
     char *filename;
