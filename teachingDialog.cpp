53a5a9dc3ab25b1262efba55027ec51a20dd4520
David Ko <david.dk.ko@gmail.com>
2013-03-11 09:22:01 -0700

    Modified generated code for pose teaching...
    For a single module, no longer use non-blocking movement functions.

--- teachingDialog.cpp
+++ teachingDialog.cpp
@@ -157,7 +157,7 @@ void on_button_saveToProgram_clicked(GtkWidget*widget, gpointer data)
       GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
       NULL);
   gtk_file_chooser_set_do_overwrite_confirmation (GTK_FILE_CHOOSER (dialog), TRUE);
-  gtk_file_chooser_set_current_name (GTK_FILE_CHOOSER (dialog), "UntitledDocument.cpp");
+  gtk_file_chooser_set_current_name (GTK_FILE_CHOOSER (dialog), "untitledprogram.ch");
   if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT)
   {
     char *filename;
@@ -344,7 +344,7 @@ void on_mobotButtonPress(void* data, int button, int buttonDown)
 
   /* Calculate the new state */
   if(buttonDown) {
-    Mobot_blinkLED(mobot, 0.1, 2);
+    //Mobot_blinkLED(mobot, 0.1, 2);
     newState = lastState | (1<<button);
   } else {
     newState = lastState & ~(1<<button);
