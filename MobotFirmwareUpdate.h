3d576c1d82d7431aaa2c22cc4e0731bfbddf4986
David Ko <david.dk.ko@gmail.com>
2013-04-04 10:02:29 -0700

    Added missing header file

---
+++ MobotFirmwareUpdate.h
@@ -0,0 +1,17 @@
+#ifndef _MOBOT_FIRMWARE_UPDATE_H_
+#define _MOBOT_FIRMWARE_UPDATE_H_
+
+#ifdef _MSYS
+#include <windows.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+G_MODULE_EXPORT void on_button_p1_next_clicked(GtkWidget* widget, gpointer data);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
